9aaab8062401278816fd83d62c1248dcb6c3adc6
Всеволод Богдашов <fishface6666@mail.ru>
2024-09-23 19:31:50 +0700

    common fix

--- main.py
+++ main.py
@@ -17,8 +17,10 @@ from PyQt6.QtWidgets import (
     QLineEdit,
     QStackedLayout,
     QTextEdit,
-    QMessageBox,
     QGridLayout,
+    QGraphicsPixmapItem,
+    QGraphicsView,
+    QGraphicsScene, QSpacerItem,
 )
 from PyQt6.QtGui import QPalette, QImage, QBrush, QPixmap, QIcon, QCursor
 
@@ -84,16 +86,18 @@ class LoginWindow(QWidget):
 class TopPanelArea(QWidget):
     def __init__(self, height, color):
         super().__init__()
-        self.background = QWidget()
-        self.background.setLayout(QHBoxLayout())
-        self.background.setStyleSheet(f"background-color: {color}; color: white;")
+        self.wrapper = QWidget()
+        self.wrapper.setLayout(QHBoxLayout())
+        self.wrapper.setStyleSheet(f"background-color: {color}; color: white;")
 
         self.setFixedHeight(height)
-        self.background.layout().addWidget(self)
+        self.wrapper.layout().addWidget(self)
+        self.wrapper.layout().setContentsMargins(0, 0, 0, 0)
 
 
 class UserNameWidget(QWidget):
     user_header = "ПОЛЬЗОВАТЕЛЬ:"
+    user_name_color = "#8c7c44"
 
     def __init__(self):
         super().__init__()
@@ -102,33 +106,28 @@ class UserNameWidget(QWidget):
         self.layout.setSpacing(0)
         self.layout.setContentsMargins(0, 0, 0, 0)
         self.user_name_header = QLabel(self.user_header)
+        self.user_name_header.setAlignment(Qt.AlignmentFlag.AlignBottom)
         self.layout.addWidget(self.user_name_header)
         self.user_name = QLabel(settings.LOGIN)
+        self.user_name.setStyleSheet(f"color: {self.user_name_color}")
+        self.user_name.setAlignment(Qt.AlignmentFlag.AlignTop)
         self.layout.addWidget(self.user_name)
-
-
-class HelpWindow(QMessageBox):
-    def __init__(self):
-        super().__init__()
-        self.setIcon(QMessageBox.Icon.Information)
-        self.setInformativeText("In develop")
-        self.setWindowTitle("Info")
-        self.setStandardButtons(QMessageBox.StandardButton.Ok)
-        self.setDefaultButton(QMessageBox.StandardButton.Ok)
 
 
 class TopPanelButtons(QWidget):
     buttons_data = (
-        ("list.PNG", "упражнения", None),
+        ("list.png", "упражнения", None),
         (
-            "question.PNG",
+            "question.png",
             "помощь",
             lambda: webbrowser.open(
                 "https://developer.mozilla.org/en-US/docs/Learn/HTML/Introduction_to_HTML/Creating_hyperlinks"
             ),
         ),
-        ("info.PNG", "о программе", lambda: HelpWindow().exec()),
-        ("exit.PNG", "выход", lambda: STACK.setCurrentIndex(0)),
+        ("info.png", "о программе", lambda: webbrowser.open(
+                "https://www.pythonguis.com/pyqt6-tutorial/"
+            )),
+        ("exit.png", "выход", lambda: STACK.setCurrentIndex(0)),
     )
 
     def __init__(self):
@@ -144,6 +143,7 @@ class TopPanelButtons(QWidget):
             button = QWidget()
             icon = TopPanelButton(filename, func)
             text_widget = QLabel(text)
+            text_widget.setAlignment(Qt.AlignmentFlag.AlignTop)
             button_layout = QVBoxLayout()
             button.setLayout(button_layout)
 
@@ -160,9 +160,9 @@ class TopPanelButton(QLabel):
         super().__init__()
         self.func = func
 
-        self.pixmap = QPixmap(str(self.base_dir / file_name)).scaled(self.size, self.size)
-
-        self.setPixmap(self.pixmap)
+        self.setPixmap(
+            QPixmap(str(self.base_dir / file_name)).scaled(self.size, self.size)
+        )
         self.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
 
@@ -172,10 +172,10 @@ class TopPanelButton(QLabel):
 
 
 class TopPanel(QWidget):
-    height = 60
+    height = 1
     base_dir = Path(BASEDIR, "icons", "main_top_panel")
-    logo_file_path = str(base_dir / "logo.PNG")
-    user_file_path = str(base_dir/ "user.PNG")
+    logo_file_path = str(base_dir / "logo.png")
+    user_file_path = str(base_dir / "user.png")
     header = "КОГНИТИВНАЯ РЕАБИЛИТАЦИЯ"
     left_panel_color = "#446c7c"
     right_panel_color = "#365b66"
@@ -189,22 +189,23 @@ class TopPanel(QWidget):
         self.layout.setContentsMargins(0, 0, 0, 0)
         self.setLayout(self.layout)
 
-        self.left_panel = TopPanelArea(self.height, self.left_panel_color).background
+        self.left_panel = TopPanelArea(self.height, self.left_panel_color).wrapper
         self.left_panel.layout().setAlignment(Qt.AlignmentFlag.AlignLeft)
         self.logo = QLabel()
-        self.logo.setPixmap(QPixmap(self.logo_file_path))
+        self.logo.setPixmap(QPixmap(self.logo_file_path).scaled(50, 50))
         self.header = QLabel(self.header)
+        self.header.setStyleSheet("font-size: 23px;")
         self.left_panel.layout().addWidget(self.logo)
         self.left_panel.layout().addWidget(self.header)
 
-        self.right_panel = TopPanelArea(self.height, self.right_panel_color).background
+        self.right_panel = TopPanelArea(self.height, self.right_panel_color).wrapper
         self.right_panel.layout().setAlignment(Qt.AlignmentFlag.AlignRight)
 
         self.user_area = QWidget()
         self.user_area.setLayout(QHBoxLayout())
         self.user_area.layout().setAlignment(Qt.AlignmentFlag.AlignLeft)
         self.user_img = QLabel()
-        self.user_img.setPixmap(QPixmap(self.user_file_path))
+        self.user_img.setPixmap(QPixmap(self.user_file_path).scaled(20, 20))
         self.user_name = UserNameWidget()
         self.user_area.layout().addWidget(self.user_img)
         self.user_area.layout().addWidget(self.user_name)
@@ -215,6 +216,7 @@ class TopPanel(QWidget):
         self.button_area.setLayout(QHBoxLayout())
         self.button_area.layout().setAlignment(Qt.AlignmentFlag.AlignRight)
         self.button_area.layout().addWidget(TopPanelButtons())
+        self.button_area.layout().setContentsMargins(0, 0, 0, 0)
         self.right_panel.layout().addStretch()
         self.right_panel.layout().addWidget(self.button_area)
 
@@ -222,28 +224,44 @@ class TopPanel(QWidget):
         self.layout.addWidget(self.right_panel, stretch=2)
 
 
-class MainIcon(QLabel):
+class MainIcon(QGraphicsView):
     animation_duration = 200
-    start_size = 70
-    end_size = 90
+    start_size = 120
+    end_size = round(start_size * 1.1)
     base_dir = Path(BASEDIR, "icons", "main_quests")
 
     def __init__(self, file_name: str):
         super().__init__()
+        self.setStyleSheet("border: none")
+        self.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
+        self.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
+        
+        self.scene = QGraphicsScene()
+        self.setScene(self.scene)
+
         self.filename = str(self.base_dir / file_name)
+
+        self.pixmap_item = QGraphicsPixmapItem()
+        self.scene.addItem(self.pixmap_item)
+        pixmap = QIcon(self.filename).pixmap(self.start_size, self.start_size)
+        self.pixmap_item.setPixmap(pixmap)
+        
         self.animation = QVariantAnimation()
         self.animation.setDuration(self.animation_duration)
         self.animation.valueChanged.connect(lambda value: self.update_pixmap(value))
+        self.animation.setStartValue(self.start_size)
+        self.animation.setEndValue(self.end_size)
 
-        self.pixmap = QPixmap(self.filename).scaled(self.start_size, self.start_size)
-
-        self.setPixmap(self.pixmap)
+        self.pixmap_item.setPos(-self.start_size / 2, -self.start_size / 2)
         self.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
 
     def update_pixmap(self, value):
         pixmap = QIcon(self.filename).pixmap(int(value), int(value))
-        self.setPixmap(pixmap)
+        pixmap_scaled = pixmap.scaled(int(value), int(value), Qt.AspectRatioMode.KeepAspectRatio,
+                                      Qt.TransformationMode.SmoothTransformation)
+        self.pixmap_item.setPixmap(pixmap_scaled)
+        self.pixmap_item.setPos(-value / 2, -value / 2)
 
     def enterEvent(self, event):
         self.animation.setStartValue(self.start_size)
@@ -265,52 +283,74 @@ class MainButton(QWidget):
     def __init__(self, file_name: str, label: str):
         super().__init__()
         self.layout = QVBoxLayout()
-        self.layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.setLayout(self.layout)
-        self.setFixedSize(MainIcon.end_size * 2, MainIcon.end_size * 2)
+        self.layout.setSpacing(0)
+        self.layout.setContentsMargins(0, 0, 0, 0)
+        self.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Fixed)
 
-        self.label = QLabel(label)
-        self.label.setStyleSheet(f"color: {self.color}")
+        self.button_header = QLabel(label)
+        self.button_header.setAlignment(
+            Qt.AlignmentFlag.AlignVCenter | Qt.AlignmentFlag.AlignHCenter
+        )
+        self.button_header.setStyleSheet(f"color: {self.color}")
 
-        self.layout.addWidget(MainIcon(file_name))
-        self.layout.addWidget(self.label)
+        self.icon = MainIcon(file_name)
+        self.icon.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        self.layout.addWidget(self.icon)
+        self.layout.addWidget(self.button_header)
 
 
 class MainBlock(QWidget):
     buttons_data = (
         (
             ("brain.PNG", "Исполнительные\nфункции"),
-            ("book.PNG", "Вербальная\nпамять"),
-            ("church.PNG", "Визуальная\nпамять"),
-            ("web.PNG", "Вербальная и\nвизуальная память"),
-            ("compass.PNG", "Пространственная\nпамять"),
+            ("map.PNG", "Навыки зрительного и\nпространственного\nвосприятия"),
         ),
         (
-            ("map.PNG", "Навыки зрительного и\nпространственного\nвосприятия"),
+            ("book.PNG", "Вербальная\nпамять"),
             ("picture.PNG", "Визуальное\nвнимание"),
+        ),
+        (
+            ("church.PNG", "Визуальная\nпамять"),
             ("clock.PNG", "Скорость обработки\nинформации"),
+        ),
+        (
+            ("web.PNG", "Вербальная и\nвизуальная память"),
             ("ear.PNG", "Слуховое\nвосприятие"),
+        ),
+        (
+            ("compass.PNG", "Пространственная\nпамять"),
             ("dialog.PNG", "Языковые навыки\nи словарный запас"),
         ),
     )
 
     def __init__(self):
         super().__init__()
-        self.layout = QVBoxLayout()
-        self.setLayout(self.layout)
-        self.layout.setAlignment(Qt.AlignmentFlag.AlignTop)
-        self.layout.setSpacing(0)
-        self.layout.setContentsMargins(0, 0, 0, 0)
+        self.setLayout(QVBoxLayout())
+        self.layout().setAlignment(Qt.AlignmentFlag.AlignTop)
+        self.layout().setSpacing(0)
+        self.layout().setContentsMargins(0, 0, 0, 0)
         self.setStyleSheet("background-color: white")
-
-        for buttons_row in self.buttons_data:
-            row = QWidget()
-            row_layout = QHBoxLayout()
-            row.setLayout(row_layout)
-            for button_data in buttons_row:
-                row_layout.addWidget(MainButton(*button_data))
-            self.layout.addWidget(row)
-        self.layout.addWidget(InfoBlock().wrapper)
+        self.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
+        
+        self.buttons_widget = QWidget()
+        self.buttons_widget.setLayout(QHBoxLayout())
+        self.layout().setAlignment(Qt.AlignmentFlag.AlignTop)
+        
+        for buttons_column in self.buttons_data:
+            column = QWidget()
+            column_layout = QVBoxLayout()
+            column.setLayout(column_layout)
+            for button_data in buttons_column:
+                button = MainButton(*button_data)
+                column_layout.addWidget(button)
+            self.buttons_widget.layout().addWidget(column)
+        
+        self.info_widget = InfoBlock().wrapper
+        
+        self.layout().addWidget(self.buttons_widget)
+        self.layout().addWidget(self.info_widget)
 
 
 class InfoBlock(QWidget):
@@ -329,24 +369,32 @@ class InfoBlock(QWidget):
         self.wrapper = QWidget()
         self.wrapper.setLayout(QVBoxLayout())
         self.wrapper.setStyleSheet("background-color: white")
-
-        self.setAutoFillBackground(True)
+        
         self.setLayout(QVBoxLayout())
         self.setStyleSheet(
             f"padding: 10px; border-radius: 3px; border: 1px solid {TopPanel.left_panel_color}; background-color: {self.background_color}"
         )
-
+        
         self.text = QTextEdit()
+        self.text.setFixedSize(1000, 150)
+        self.text.setStyleSheet("padding: 10px")
+        self.text.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.text.setReadOnly(True)
         self.text.setHtml(
             """
-            <p>{}</p>
-            <p>{}</p>
-        """.format(
+                <p>{}</p>
+                <p>{}</p>
+            """.format(
                 self.info_header, self.info
             )
         )
-        self.layout().addWidget(self.text)
+        
+        h_layout = QHBoxLayout()
+        h_layout.addStretch(1)
+        h_layout.addWidget(self.text)
+        h_layout.addStretch(1)
+        
+        self.layout().addLayout(h_layout)
         self.wrapper.layout().addWidget(self)
 
 
@@ -354,6 +402,7 @@ class Texture(QWidget):
     texture_file_path = str(Path(BASEDIR, "icons", "texture.jpg").resolve())
     header = "ГРУППЫ УПРАЖНЕНИЙ"
     header_color = TopPanel.left_panel_color
+
     def __init__(self):
         super().__init__()
         self.layout = QVBoxLayout()
@@ -372,8 +421,9 @@ class Texture(QWidget):
 
         self.header_widget = QLabel(self.header)
         self.header_widget.setStyleSheet(
-            f"font-size: 25px; color: {self.header_color}; letter-spacing: 1px; font-weight 500;"
+            f"font-size: 25px; color: {self.header_color}; font-weight 500;"
         )
+        self.header_widget.setContentsMargins(0, 30, 0, 0)
         self.layout.addWidget(self.header_widget, alignment=Qt.AlignmentFlag.AlignTop)
         self.layout.addWidget(MainBlock())
 
@@ -392,6 +442,7 @@ class MenuWindow(QWidget):
 class QuestWindow(QWidget):
     header = "Запомните изображения и их расположение"
     button_text = "Продолжить"
+    back_text = "Назад"
     quest_size = 3
     icons_data = {
         (0, 0): "bag.PNG",
@@ -402,20 +453,21 @@ class QuestWindow(QWidget):
     }
     button_color = "#499477"
     base_dir = Path(BASEDIR, "icons", "main_quests", "generic")
-
+    arrow_icon_path = str(Path(base_dir, "left-arrow.png").resolve())
+    
     def __init__(self):
         super().__init__()
         self.setLayout(QVBoxLayout())
         self.header = QLabel(self.header)
-        self.header.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.header.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignBottom)
         self.layout().addWidget(self.header)
         self.layout().setAlignment(Qt.AlignmentFlag.AlignCenter)
-
+        
         self.quest_content = QWidget()
         self.quest_content.setLayout(QGridLayout())
         self.quest_content.layout().setSpacing(0)
-
-        for i in range(self.quest_size**2):
+        
+        for i in range(self.quest_size ** 2):
             x = i % self.quest_size
             y = i // self.quest_size
             file_name = self.icons_data.get((y, x))
@@ -426,23 +478,44 @@ class QuestWindow(QWidget):
             icon.setStyleSheet(f"border: 1px solid black")
             icon.setFixedSize(QSize(300, 150))
             self.quest_content.layout().addWidget(icon, y, x)
+        
+        self.back = QWidget()
+        self.back.setLayout(QHBoxLayout())
+        self.back.layout().setAlignment(Qt.AlignmentFlag.AlignLeft)
 
+        self.arrow_widget = QLabel()
+        self.arrow_widget.setFixedSize(20, 20)
+        self.arrow_widget.setPixmap(QPixmap(self.arrow_icon_path).scaled(20, 20))
+
+        self.arrow_widget.setAlignment(Qt.AlignmentFlag.AlignLeft)
+        self.back.layout().addWidget(self.arrow_widget)
+        self.back_button = QPushButton(self.back_text)
+        self.back_button.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
+        self.back_button.setStyleSheet(f"border: none")
+        self.back_button.setFixedSize(50, 20)
+        self.back_button.clicked.connect(lambda: STACK.setCurrentIndex(1))
+        self.back.layout().addWidget(self.back_button)
+        
         self.button_continue = QPushButton(self.button_text)
         self.button_continue.clicked.connect(lambda: STACK.setCurrentIndex(1))
         self.button_continue.setStyleSheet(
             f"background-color: {self.button_color}; color: white"
         )
-        self.button_continue.setSizePolicy(
-            QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed
-        )
         self.layout().addWidget(self.quest_content)
-
+        
         self.wrapper = QWidget()
         self.wrapper.setLayout(QHBoxLayout())
         self.wrapper.setStyleSheet("background-color: white")
         self.wrapper.layout().addWidget(self)
         self.wrapper.layout().addWidget(self.button_continue)
         self.wrapper.layout().setAlignment(Qt.AlignmentFlag.AlignCenter)
+        
+        self.wrapper_2 = QWidget()
+        self.wrapper_2.setLayout(QVBoxLayout())
+        self.wrapper_2.layout().setAlignment(Qt.AlignmentFlag.AlignTop)
+
+        self.wrapper_2.layout().addWidget(self.back)
+        self.wrapper_2.layout().addWidget(self.wrapper)
 
 
 class MainWindow(QMainWindow):
@@ -456,8 +529,8 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(self.index)
         STACK.addWidget(LoginWindow())
         STACK.addWidget(MenuWindow())
-        STACK.addWidget(QuestWindow().wrapper)
-        STACK.setCurrentIndex(0)
+        STACK.addWidget(QuestWindow().wrapper_2)
+        STACK.setCurrentIndex(1)
 
 
 def main():
